c2891c47dafc4f8e54b66b05857ec644e2ac6fee
Coder-579 <susers6edge@gmail.com>
2018-11-18 15:50:36 +0600

    this

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hey command')
+//this is just simple example of timer
+
+// var time = 0;
+
+// var timer = setInterval(function(){
+//   time +=2;
+//   console.log(time + ' seconds')
+//   if( time > 5) {
+//     clearInterval(timer)
+//   }
+// }, 3000);
+
+
+//This will in which directory i am 
+console.log(__dirname)
+//This will in which directory i am 
+console.log(__filename+'filename ')
