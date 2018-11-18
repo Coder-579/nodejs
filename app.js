528c17c003cb50f34f575a1ed70a363463be7f70
Coder-579 <susers6edge@gmail.com>
2018-11-18 16:05:21 +0600

    tutorial-5

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-//this is just simple example of timer
+//function expression
 
-// var time = 0;
+var sayBye = function(){
+  console.log('bye');
+}
 
-// var timer = setInterval(function(){
-//   time +=2;
-//   console.log(time + ' seconds')
-//   if( time > 5) {
-//     clearInterval(timer)
-//   }
-// }, 3000);
+sayBye();
 
+//Callback function
 
-//This will in which directory i am 
-console.log(__dirname)
-//This will in which directory i am 
-console.log(__filename+'filename ')
+function callFunction(fun){
+  fun();
+}
+
+callFunction(sayBye);
