e15f1c0a91f3b2d04dd6dc04492f50c3c6c78796
Coder-579 <susers6edge@gmail.com>
2018-11-19 15:43:42 +0600

    new things

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-var fs = require('fs');
+var http = require('http')
+var fs = require('fs')
 
-//delete file
-// fs.unlink('writeme.txT')
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
 
-//creating directory synchronus
-// fs.mkdirSync('stuff')
-//Delete directory
-// fs.rmdirSync('stuff')
-//Asynchronus method to delete directory
-// fs.mkdir('stuff', function(){
-//   fs.readFile('readme.txt', 'utf8', function( err, data){
-//     fs.writeFile('./stuff/writeME.txt', data);
-//   })
-// })
-
-//Remove file then remove's the directory
-// fs.unlink('./stuff/writeme.txt',function(){
-//   fs.rmdir('stuff')
-// })
-// fs.rmdir('stuff')
+myReadStream.on('data', function(chunk){
+  console.log('new chunk: ')
+  console.log(chunk)
+})
