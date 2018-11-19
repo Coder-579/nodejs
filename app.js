3e16b86775a70769a5b8555f2abbba9995bc8c1a
Coder-579 <susers6edge@gmail.com>
2018-11-19 15:54:28 +0600

    it can write and read

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var http = require('http')
 var fs = require('fs')
 
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
+var myWriteStram = fs.createWriteStream(__dirname + '/writeme.txt')
 
 myReadStream.on('data', function(chunk){
   console.log('new chunk: ')
-  console.log(chunk)
+  myWriteStram.write(chunk)
 })
