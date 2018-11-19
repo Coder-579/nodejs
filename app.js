0f3bcafc6d7ec8c6c29a658792a94f46cb64a4c8
Coder-579 <susers6edge@gmail.com>
2018-11-19 16:33:18 +0600

    this can now show json

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ var fs = require('fs')
 var server = http.createServer(function(req, res){
   // console.log(req,'there was req now you will see res ', res)
   console.log('req was made: ' + req.url)
-  res.writeHead(200, {'Content-Type': 'text/htmL'})
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
-  myReadStream.pipe(res)
+  res.writeHead(200, {'Content-Type': 'application/json'})
+  var myObj = {
+    name: 'Ryan',
+    job: 'Ninja',
+    age: 590
+  }
+  res.end(JSON.stringify(myObj))
   // res.end('Hey ninjas!!!')
 })
 
