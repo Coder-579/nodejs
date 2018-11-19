7570565b5cf54bcaf5e82985d7775326db1635c7
Coder-579 <susers6edge@gmail.com>
2018-11-19 16:14:45 +0600

    served html to browser

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var http = require('http')
 var fs = require('fs')
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
-var myWriteStram = fs.createWriteStream(__dirname + '/writeme.txt')
+// var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
+// var myWriteStram = fs.createWriteStream(__dirname + '/writeme.txt')
 
-myReadStream.on('data', function(chunk){
-  console.log('new chunk: ')
-  myWriteStram.write(chunk)
+// myReadStream.pipe(myWriteStram)
+
+
+var server = http.createServer(function(req, res){
+  // console.log(req,'there was req now you will see res ', res)
+  console.log('req was made: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'text/htmL'})
+  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+  myReadStream.pipe(res)
+  // res.end('Hey ninjas!!!')
 })
+
+server.listen(3000, '127.0.0.1')
+
+console.log('listening to port 3000')
