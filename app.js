437d0e25d276d2a644a0d007f907bc49f6bb65f9
Coder-579 <susers6edge@gmail.com>
2018-11-19 16:57:29 +0600

    this now has routing capability

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var http = require('http')
 var fs = require('fs')
 
-// var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
-// var myWriteStram = fs.createWriteStream(__dirname + '/writeme.txt')
-
-// myReadStream.pipe(myWriteStram)
-
-
 var server = http.createServer(function(req, res){
   // console.log(req,'there was req now you will see res ', res)
-  console.log('req was made: ' + req.url)
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  var myObj = {
-    name: 'Ryan',
-    job: 'Ninja',
-    age: 590
+  console.log('reques was made ' + req.url)
+  if (req.url === '/home' || req.url === '/'){
+    res.writeHead(200, {'Content-Type' : 'text/html'})
+    fs.createReadStream(__dirname + '/index.html').pipe(res)
+  } else if ( req.url === '/contact'){
+    res.writeHead(200, {'Content-Type' : 'text/html'})
+    fs.createReadStream(__dirname + '/contact.html').pipe(res)
+  } else if (req.url === '/api/ninjas') {
+    var ninjas = [{name:'base', age: 78}, {name: 'An', age: 89}]
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(ninjas))
+  } else {
+    res.writeHead(404, {'Content-Type' : 'text/html'})
+    fs.createReadStream(__dirname + '/404.html').pipe(res)
   }
-  res.end(JSON.stringify(myObj))
-  // res.end('Hey ninjas!!!')
 })
 
 server.listen(3000, '127.0.0.1')
